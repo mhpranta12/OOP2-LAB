cfff9ad4de77d0f1814dd2c938bfb8614a6ef178
mh pranta <mhpranta2@gmail.com>
2020-10-19 13:55:04 +0600

    new

---
+++ Lab 1 Problem 13/ConsoleApp2/Lab 1 Problem 13.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x =1,l=5;
+            for (int j = 0; j < 5; j++)
+            {
+                for (int i = x; i <= l; i++)
+                {
+                    Console.Write(i);
+
+
+
+
+                }
+                Console.WriteLine();
+                x++;
+                l++;
+            }
+        }
+    }
+}
